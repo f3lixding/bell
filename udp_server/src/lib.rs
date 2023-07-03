1550e0c6ed394cfb5aca6a6e0b5bbb9d2179ad6e
Felix Ding <felixding@Felixs-MacBook-Pro.local>
2023-07-02 19:04:38 -0700

    Adds one event loop to flush queue and one for queueing

--- udp_server/src/lib.rs
+++ udp_server/src/lib.rs
@@ -1,7 +1,65 @@
+use enum_dispatch::enum_dispatch;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Default, Debug, Clone)]
 pub struct Point {
     pub x: f32,
     pub y: f32,
+}
+
+#[enum_dispatch]
+pub trait GameStateMessage {
+    fn open(&self) -> (f32, f32);
+}
+
+#[enum_dispatch(GameStateMessage)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub enum BellMessage {
+    PositionChangeMessage,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PositionChangeMessage {
+    pub x: f32,
+    pub y: f32,
+}
+impl GameStateMessage for PositionChangeMessage {
+    fn open(&self) -> (f32, f32) {
+        (self.x, self.y)
+    }
+}
+
+pub struct GameState {
+    capacity: usize,
+    process_queue: Vec<Option<BellMessage>>,
+}
+
+impl GameState {
+    pub fn new_with_capacity(capacity: usize) -> Self {
+        Self {
+            capacity,
+            process_queue: Vec::<Option<BellMessage>>::with_capacity(capacity),
+        }
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.process_queue.len() >= self.capacity
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.process_queue.is_empty()
+    }
+
+    pub fn queue_message(&mut self, message: BellMessage) {
+        self.process_queue.push(Some(message));
+    }
+
+    pub fn retrieve_messages(&mut self) -> Vec<BellMessage> {
+        let mut messages = Vec::<BellMessage>::with_capacity(self.process_queue.len());
+        while !self.process_queue.is_empty() {
+            messages.push(self.process_queue.pop().unwrap().unwrap());
+        }
+
+        messages
+    }
 }
--- udp_server/src/main.rs
+++ udp_server/src/main.rs
@@ -1,24 +1,67 @@
+use lib_udp_server::{BellMessage, GameState, Point, PositionChangeMessage};
+use std::sync::Arc;
 use tokio::net::UdpSocket;
-use lib_udp_server::Point;
+use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Program started");
+
+    let game_state = GameState::new_with_capacity(1000);
+    let game_state = Arc::new(RwLock::new(game_state));
+
+    let rest_period_in_secs = 3;
+
+    let game_state_clone = game_state.clone();
+    tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(std::time::Duration::from_secs(rest_period_in_secs)).await;
+            let is_empty = {
+                let game_state = game_state_clone.read().await;
+                game_state.is_empty()
+            };
+
+            if !is_empty {
+                let mut game_state = game_state_clone.write().await;
+                let messages = game_state.retrieve_messages();
+                tokio::spawn(async move {
+                    println!("{} valid message received and should be processed here", messages.len());
+                });
+            }
+        }
+    });
+
+    let game_state_clone = game_state.clone();
     let socket = UdpSocket::bind("127.0.0.1:8080").await?;
     let mut buf = vec![0; 1024];
 
+    // using RwLock for now
     while let Ok((size, src)) = socket.recv_from(&mut buf).await {
-        let data = &buf[..size];
-        println!("Received {} bytes from {}", size, src);
-        let mut point = serde_json::from_slice::<Point>(&data).unwrap_or_default();
-        println!("Received a point: {:?}", point);
-        if point.x > 220.0 {
-            point.x = 120.0;
-            let point_byte = serde_json::to_vec(&point).unwrap();
-            socket.send_to(&point_byte, src).await?;
+        {
+            let sample_data = BellMessage::from(PositionChangeMessage { x: 0.0, y: 0.0 });
+            let mut data = serde_json::to_vec(&sample_data).unwrap();
+            data.push(b'\n');
+            socket.send_to(&data, src).await?;
         }
-//         point_byte.push("\n".as_bytes()[0]);
-//         socket.send_to(&point_byte, src).await?;
+        let mut data = vec![0; size];
+        data.copy_from_slice(&buf[..size]);
+        let data = serde_json::from_slice::<BellMessage>(&data);
+        if data.is_ok() {
+            let data = data.unwrap();
+            let is_full = {
+                let game_state = game_state_clone.read().await;
+                game_state.is_full()
+            };
+
+            if !is_full {
+                let mut game_state = game_state_clone.write().await;
+                game_state.queue_message(data);
+            } else {
+                println!("Game is full"); // TODO: need to send a message to the client that the game is full
+            }
+        } else {
+            println!("Message received isn't BellMessage");
+        }
     }
 
     Ok(())
