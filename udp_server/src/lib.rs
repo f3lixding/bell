ae07739f52c466715abae0a1ce0aecc20b8238c6
Felix Ding <felixding1989@gmail.com>
2023-07-05 20:17:25 -0700

    Adds player registration logic in server

--- udp_server/src/lib.rs
+++ udp_server/src/lib.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 pub struct Point {
     pub x: f32,
     pub y: f32,
-    pub id: i32,
+    pub id: u32,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -12,25 +12,26 @@ pub enum BellMessage {
     PositionChangeMessage(Point),
     DeferMessage,
     PlayerInsertionMessage(Point),
+    PlayerRegistrationMessage(Point),
 }
 
 pub struct GameState {
     capacity: usize,
     process_queue: Vec<Option<BellMessage>>,
-    _positions: std::collections::HashMap<i32, (f32, f32)>,
-    addrs: std::collections::HashMap<i32, std::net::SocketAddr>,
+    _positions: std::collections::HashMap<u32, (f32, f32)>,
+    addrs: std::collections::HashMap<u32, std::net::SocketAddr>,
 }
 impl GameState {
     pub fn new_with_capacity(capacity: usize) -> Self {
         Self {
             capacity,
             process_queue: Vec::<Option<BellMessage>>::with_capacity(capacity),
-            _positions: std::collections::HashMap::<i32, (f32, f32)>::with_capacity(2),
-            addrs: std::collections::HashMap::<i32, std::net::SocketAddr>::with_capacity(2),
+            _positions: std::collections::HashMap::<u32, (f32, f32)>::with_capacity(2),
+            addrs: std::collections::HashMap::<u32, std::net::SocketAddr>::with_capacity(2),
         }
     }
 
-    pub fn insert_player(&mut self, id: i32, x: f32, y: f32, addr: std::net::SocketAddr) {
+    pub fn insert_player(&mut self, id: u32, x: f32, y: f32, addr: std::net::SocketAddr) {
         self.addrs.insert(id, addr);
         self._positions.insert(id, (x, y));
     }
@@ -55,16 +56,17 @@ impl GameState {
         messages
     }
 
-    pub fn get_collided_pairs(&self) -> Vec<(i32, i32)> {
+    pub fn get_collided_pairs(&self) -> Vec<(u32, u32)> {
         vec![]
     }
 
-    pub fn get_addr_from_id(&self, id: i32) -> Option<&std::net::SocketAddr> {
+    pub fn get_addr_from_id(&self, id: u32) -> Option<&std::net::SocketAddr> {
         self.addrs.get(&id)
     }
 
-    pub fn get_addrs_for_id(&self, id: i32) -> Vec<&std::net::SocketAddr> {
+    pub fn get_addrs_for_id(&self, id: u32) -> Vec<&std::net::SocketAddr> {
         let mut res_addrs = Vec::with_capacity(3);
+        println!("addrs: {:?}", self.addrs);
         for (k, v) in self.addrs.iter() {
             if k != &id {
                 res_addrs.push(v);
--- udp_server/src/main.rs
+++ udp_server/src/main.rs
@@ -1,4 +1,6 @@
-use lib_udp_server::{BellMessage, GameState, Point};
+use lib_udp_server::{BellMessage, GameState};
+use std::io::Write;
+use std::sync::atomic::AtomicU32;
 use std::sync::Arc;
 use tokio::net::UdpSocket;
 use tokio::sync::RwLock;
@@ -10,16 +12,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let game_state = GameState::new_with_capacity(1000);
     let game_state = Arc::new(RwLock::new(game_state));
 
-    let rest_period_in_secs = 3;
+    let rest_period = 16;
 
     let socket = UdpSocket::bind("127.0.0.1:8080").await?;
     let socket = Arc::new(socket);
 
     let socket_clone = socket.clone();
     let game_state_clone = game_state.clone();
+
+    // Loop for flushing message queue
     tokio::spawn(async move {
         loop {
-            tokio::time::sleep(std::time::Duration::from_secs(rest_period_in_secs)).await;
+            tokio::time::sleep(std::time::Duration::from_millis(rest_period)).await;
             let is_empty = {
                 let game_state = game_state_clone.read().await;
                 game_state.is_empty()
@@ -40,6 +44,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .iter()
                         .map(|message| match message {
                             BellMessage::PositionChangeMessage(point) => {
+                                println!("Processing position change message");
                                 let audiences = game_state.get_addrs_for_id(point.id);
                                 audiences
                                     .iter()
@@ -48,7 +53,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                     })
                                     .collect::<Vec<(&std::net::SocketAddr, BellMessage)>>()
                             }
-                            BellMessage::PlayerInsertionMessage(point) => {
+                            BellMessage::PlayerRegistrationMessage(point) => {
+                                println!("Processing player registration message");
                                 let audiences = game_state.get_addrs_for_id(point.id);
                                 audiences
                                     .iter()
@@ -63,6 +69,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .collect::<Vec<(&std::net::SocketAddr, BellMessage)>>();
 
                     for (addr, message) in out_going_messages {
+                        println!("Sending message to {}", addr);
                         let data = serde_json::to_vec(&message).unwrap();
                         _ = socket.send_to(&data, *addr).await;
                     }
@@ -75,22 +82,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = vec![0; 1024];
 
     // using RwLock for now
+    // Loop for listening for incoming udp packets
+    let next_available_id = Arc::new(AtomicU32::new(0));
     while let Ok((size, src)) = socket.recv_from(&mut buf).await {
-        {
-            let sample_data = BellMessage::PositionChangeMessage(Point {
-                x: 0.,
-                y: 0.,
-                id: 0,
-            });
-            let mut data = serde_json::to_vec(&sample_data).unwrap();
-            data.push(b'\n');
-            socket.send_to(&data, src).await?;
-        }
+        println!("Received {} bytes from {}", size, src);
         let mut data = vec![0; size];
         data.copy_from_slice(&buf[..size]);
         let data = serde_json::from_slice::<BellMessage>(&data);
         if data.is_ok() {
-            let data = data.unwrap();
+            let mut data = data.unwrap();
             let is_full = {
                 let game_state = game_state_clone.read().await;
                 game_state.is_full()
@@ -98,8 +98,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             if !is_full {
                 let mut game_state = game_state_clone.write().await;
-                if let BellMessage::PlayerInsertionMessage(ref point) = data {
-                    game_state.insert_player(point.id, point.x, point.y, src);
+                if let BellMessage::PlayerRegistrationMessage(ref mut point) = data {
+                    let id = next_available_id.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                    point.id = id;
+                    game_state.insert_player(id, point.x, point.y, src);
+                    let mut data_buf = [0u8; 4];
+                    data_buf.as_mut().write(&id.to_be_bytes()).unwrap();
+                    _ = socket.send_to(&data_buf, src).await;
                 }
                 game_state.queue_message(data);
             } else {
